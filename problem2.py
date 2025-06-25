8a8a9ec121d3715b0c04b5c682429898acb0a22d
Mohamed Asif <rmdasif.786@gmail.com>
2025-06-25 22:30:00 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,18 @@
+number = int(input("Enter your number: "))
+print("Your number is " + str(number))
+
+if number % 4 ==0:
+    print("Your number is multiple of 4")
+elif number % 2 ==0:
+    print("number is even")
+else:
+    print("Your number is odd")
+
+num = int(input("Enter your num: "))
+check = int(input("Enter your check: "))
+
+if num % check ==0:
+    print("check evenly divides num")
+else:
+    print("does not divide")
+
